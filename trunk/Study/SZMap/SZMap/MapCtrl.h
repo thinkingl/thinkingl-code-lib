b2bb930782d09426264c17de86833ffae76857a8
thinkingzx <thinkingzx@076e9d56-d92b-0410-8186-f370c9e58ff5>
2010-08-13 10:31:43 +0000

    szmap ： 实现图片随着鼠标运动。

--- trunk/Study/SZMap/SZMap/MapCtrl.h
+++ trunk/Study/SZMap/SZMap/MapCtrl.h
@@ -15,11 +15,18 @@ private:
 	/** 图像绘制。 */
 	CImage m_imgDraw;
 
+	/** 图片左上角位置。 */
+	CPoint m_imgPos;
+
+	/** 鼠标上次位置。 */
+	CPoint m_lastMousePos;
+
 protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnPaint();
 	virtual void PreSubclassWindow();
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
 };
 
 
