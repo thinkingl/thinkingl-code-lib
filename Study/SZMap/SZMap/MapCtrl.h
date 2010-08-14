1e738d131ecf057d2d4d5f637abc763d79f88b88
thinkingzx <thinkingzx@076e9d56-d92b-0410-8186-f370c9e58ff5>
2010-08-14 10:52:36 +0000

    szmap : 图片的无限自动载入，拼接，拖动。 下一步实现自动销毁图片。

--- Study/SZMap/SZMap/MapCtrl.h
+++ Study/SZMap/SZMap/MapCtrl.h
@@ -1,6 +1,6 @@
 #pragma once
 
-
+#include <map>
 // CMapCtrl
 
 class CMapCtrl : public CWnd
@@ -11,15 +11,56 @@ public:
 	CMapCtrl();
 	virtual ~CMapCtrl();
 
-private:
-	/** 图像绘制。 */
-	CImage m_imgDraw;
+	enum
+	{
+		IMG_SIZE = 256,
+	};
 
-	/** 图片左上角位置。 */
-	CPoint m_imgPos;
+private:
+	/** 刷新图片。 */
+	void UpdateImageBuffer();
+private:
 
 	/** 鼠标上次位置。 */
 	CPoint m_lastMousePos;
+
+	/** 显示的图片序号。 */
+	CRect m_imageIndexRect;
+
+	/** 显示区域 */
+	CRect m_rectShow;
+
+	/** 图片区域左上角 */
+	CPoint m_ptImage;
+
+	/** 新生图片区域，如果图片区域切入此区域，则需要扩充图片区域，直到大于此区域。 */
+	CRect m_rectNewImage;
+	/** 删除图片区域，图片区域一旦超出此区域，则消减图片区域，直到小于此区域。 */
+	CRect m_rectDead;
+
+	/** 当前显示的图片和位置表。 */
+//	typedef CPoint TImageIndex;
+	class CImageIndex : public CPoint
+	{
+	public:
+		bool operator<( const CImageIndex& another ) const
+		{
+			if( this->x == another.x )
+			{
+				return this->y  < another.y;
+			}
+			else
+			{
+				return this->x < another.x;
+			}
+		}
+	};
+
+
+	typedef std::auto_ptr< CImage > PTImage;
+	typedef std::map< CImageIndex, PTImage> TImageBufferTable;
+
+	TImageBufferTable m_tableImageBuffer;
 
 protected:
 	DECLARE_MESSAGE_MAP()
@@ -27,6 +68,7 @@ public:
 	afx_msg void OnPaint();
 	virtual void PreSubclassWindow();
 	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
 };
 
 
