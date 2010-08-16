39b879843ddcfc1a98464a015f1649f1a01653be
thinkingzx <thinkingzx@076e9d56-d92b-0410-8186-f370c9e58ff5>
2010-08-16 06:31:06 +0000

    szmap : 实现已经下载的电子地图的显示。 实现没有图片的区域的显示没有图片的提示文字。

--- Study/SZMap/SZMap/MapCtrl.h
+++ Study/SZMap/SZMap/MapCtrl.h
@@ -3,6 +3,23 @@
 #include <map>
 // CMapCtrl
 
+/** 当前显示的图片和位置表。 */
+class CImageIndex : public CPoint
+{
+public:
+	bool operator<( const CImageIndex& another ) const
+	{
+		if( this->x == another.x )
+		{
+			return this->y  < another.y;
+		}
+		else
+		{
+			return this->x < another.x;
+		}
+	}
+};
+
 class CMapCtrl : public CWnd
 {
 	DECLARE_DYNAMIC(CMapCtrl)
@@ -19,6 +36,12 @@ public:
 private:
 	/** 刷新图片。 */
 	void UpdateImageBuffer();
+
+	/** 载入图片。 */
+	BOOL LoadMapImage( const CImageIndex& imgIndex );
+
+	/** 通过图片序号，定位图片文件。 */
+	CString ImageIndex2ImagePath( int zLevel, const CImageIndex& imgIndex ) const;
 private:
 
 	/** 鼠标上次位置。 */
@@ -38,29 +61,16 @@ private:
 	/** 删除图片区域，图片区域一旦超出此区域，则消减图片区域，直到小于此区域。 */
 	CRect m_rectDead;
 
-	/** 当前显示的图片和位置表。 */
-//	typedef CPoint TImageIndex;
-	class CImageIndex : public CPoint
-	{
-	public:
-		bool operator<( const CImageIndex& another ) const
-		{
-			if( this->x == another.x )
-			{
-				return this->y  < another.y;
-			}
-			else
-			{
-				return this->x < another.x;
-			}
-		}
-	};
+
 
 
 	typedef std::auto_ptr< CImage > PTImage;
 	typedef std::map< CImageIndex, PTImage> TImageBufferTable;
 
 	TImageBufferTable m_tableImageBuffer;
+
+	/** 当前的缩放等级 */
+	int m_nZLevel;
 
 protected:
 	DECLARE_MESSAGE_MAP()
