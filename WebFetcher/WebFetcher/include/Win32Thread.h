db8b4930c604c88491c2861d9c118926b00d49b8
thinkingzx <thinkingzx@076e9d56-d92b-0410-8186-f370c9e58ff5>
2010-08-23 02:48:31 +0000

    szmap : 工程配置修改。

--- WebFetcher/WebFetcher/include/Win32Thread.h
+++ WebFetcher/WebFetcher/include/Win32Thread.h
@@ -13,8 +13,10 @@ public:
 	/** 停止线程时的超时时间。 */
 	virtual void SetTimeOut( uint32 nMilliSeconds ) ;
 
-	/** 运行。 */
-	virtual BOOL Start() ;
+	/** 运行。 
+	*	LPCTSTR lpstrLogFolder : 线程日志路径，如果为NULL，则使用程序的当前目录。
+	*/
+	virtual BOOL Start( LPCTSTR lpstrLogFolder = NULL ) ;
 	/** 同步等待停止。*/
 	virtual BOOL Stop() ;
 
@@ -26,16 +28,22 @@ public:
 protected:
 	virtual BOOL GetStopFlag();
 private:
+	/** 获取日志目录。 */
+	tstring GetLogFolder() const;
+
 	static DWORD WINAPI FunThreadS( void * param );
 
 	int FunThread();
 
+private:
 	HANDLE m_hThread;
 
 	volatile BOOL m_bRun;
 
 	uint32 m_nTimeoutMilliseconds;
 
-	CWin32Mutex m_threadSafeLock;
+	mutable CWin32Mutex m_threadSafeLock;
 
+	/** 日志文件夹路径。 */
+	tstring m_strLogFolder;
 };
