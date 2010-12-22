741e1893f7873b77404fbf9fd52ad11aa90f73f2
thinkingzx <thinkingzx@076e9d56-d92b-0410-8186-f370c9e58ff5>
2010-12-22 12:57:27 +0000

    usaco : cryptcowgraphy

--- OnlineJudge/USACO_1/Cryptcowgraphy/cryptcowgraphy.cpp
+++ OnlineJudge/USACO_1/Cryptcowgraphy/cryptcowgraphy.cpp
@@ -38,7 +38,9 @@ SAMPLE OUTPUT (file cryptcow.out)
 */
 
 /** 
-*	
+*	深度优先搜索.
+*	关键是剪枝优化,用了哈希.
+*	哈希的大小会影响时间和结果正确性.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -79,7 +81,245 @@ typedef unsigned long u32;
 #define THINKINGL 1
 #endif
 
+const int MaxCharacterNum = 75;
+string g_OriginalMessage = "Begin the Escape execution at the Break of Dawn";
+const int PlanCharacterNum = g_OriginalMessage.length();
 
+const int HASH_TABLE_SIZE = 802973;
+
+bool *g_HashTable = 0;
+
+bool CheckMessageValid( const char* pMessage )
+{
+	char szTmp[ MaxCharacterNum + 1 ] = {0};
+	int nCursor = 0;
+	for ( ; *pMessage; ++pMessage )
+	{
+		switch ( *pMessage )
+		{
+		case 'C':
+		case 'O':
+		case 'W':
+			if ( nCursor > 0 )
+			{
+				if ( g_OriginalMessage.find( szTmp ) == -1 )
+				{
+					return false;
+				}
+				memset( szTmp, 0, sizeof( szTmp ) );
+				nCursor = 0;
+			}
+			break;
+		default:
+			szTmp[ nCursor ] = *pMessage;
+			++nCursor;
+			break;
+		}
+	}
+	return true;
+}
+
+int ELFHash( const char *pStr )
+{
+	unsigned long h=0,g;
+	while(*pStr)
+	{
+		h=(h<<4) + *pStr++;
+		g=h & 0xF0000000L;
+		if(g) h^= g>>24;
+		h &= ~g;
+	}
+	return h % HASH_TABLE_SIZE;
+}
+
+/** 搜索.
+*	
+*/
+bool dfs( const char * pCrypt, const char* pOriginalBegin, const char *pOriginalEnd )
+{
+	int nHashValue = ELFHash( pCrypt );
+
+	if ( !g_HashTable[ nHashValue ] )
+	{
+		return false;
+	}
+
+	bool bResult = true;
+
+	// 校验.
+	if ( !CheckMessageValid( pCrypt ) )
+	{
+		g_HashTable[ nHashValue ] = false;
+		return false;
+	}
+
+	const char* pC = pCrypt;
+	const char *pFirstC = 0;
+	const char *pLastW = 0;
+
+	for ( ; *pC; ++pC )		// 尝试不同的C
+	{
+		if ( ( 0 == pFirstC ) )	// 在遇到第一个'C'之前.
+		{
+			if ( 'C' == *pC )
+			{
+				pFirstC = pC;
+			}
+			else 
+			{
+				if ( *pC != *pOriginalBegin )
+				{
+					g_HashTable[ nHashValue ] = false;
+
+					return false;	// 第一个C之前的字符必须和原始字符相符.
+				}
+				++ pOriginalBegin;
+			}
+			
+		}
+		if ( *pC == 'C' )			
+		{	
+			for ( const char* pW= pCrypt+ strlen( pCrypt ) -1; pW > pC; --pW )	// 尝试O后面不同的W.从字符串的末尾开始找'W'
+			{
+				if ( 0 == pLastW )	// 找最后一个W.
+				{
+					if ( 'W' == *pW )
+					{
+						pLastW = pW;
+					}
+					else
+					{
+						if ( *pW != *pOriginalEnd ) // 在找到最后一个'W'之前,要和原始字符相符.
+						{
+							g_HashTable[ nHashValue ] = false;
+
+							return false;
+						}
+						-- pOriginalEnd;
+					}
+				}
+				if ( 'W'== *pW )
+				{				
+					for ( const char* pO = pC+1; pO < pW; ++pO )	// 尝试C后面不同的O
+					{
+						if ( 'O' == *pO )	
+						{
+							// 进行解码.
+							char planMessage[MaxCharacterNum]={0};
+							int nCursor = 0;
+							const char* pOrM = g_OriginalMessage.c_str();
+
+							// 从第一个C开始,第一个C之前的部分被削去.
+							for ( const char* pMessage = pFirstC; pMessage<pC; ++pMessage )	// first C to C
+							{								
+								planMessage[ nCursor++ ] = ( *pMessage );
+							}
+							for ( const char* pM = pO+1; pM<pW; ++pM ) // O to W
+							{
+								planMessage[ nCursor++ ] = ( *pM );
+							}
+							for ( const char* pM = pC+1; pM<pO; ++pM )	// C to O
+							{
+								planMessage[ nCursor++ ] = ( *pM );
+							}
+							for ( const char* pM = pW+1; pM <= pLastW; ++pM )	// W to LastW.
+							{
+								planMessage[ nCursor++ ] = ( *pM );
+							}
+							
+							// 进行下一轮的校验.
+							bResult = dfs( planMessage, pOriginalBegin, pOriginalEnd );
+							if ( bResult )
+							{
+								return true;
+							}
+
+						}
+					}
+							
+				}
+			}
+						
+		}
+	}
+
+	
+	return bResult;	
+}
+
+/** 尝试重新编码,不行,废弃. */
+bool Recode( string& crypt, string& original )
+{
+	const char* pMessage = crypt.c_str();
+
+	char nextCode = '0';
+	typedef std::map< string, char > TCodeTable;
+	TCodeTable codeTable;
+
+	string strCodedCrypt;
+	char szTmp[ MaxCharacterNum + 1 ] = {0};
+	int nCursor = 0;
+	for ( ; *pMessage; ++pMessage )
+	{
+		switch ( *pMessage )
+		{
+		case 'C':
+		case 'O':
+		case 'W':
+			if ( nCursor > 0 )
+			{
+				if ( codeTable.find( szTmp ) == codeTable.end() )				
+				{
+					codeTable[ szTmp ] = nextCode;
+					++nextCode;
+				}
+				
+				strCodedCrypt.push_back( codeTable[ szTmp ] );
+
+				memset( szTmp, 0, sizeof( szTmp ) );
+				nCursor = 0;
+			}
+
+			strCodedCrypt.push_back( *pMessage );
+			break;
+		default:
+			szTmp[ nCursor ] = *pMessage;
+			++nCursor;
+			break;
+		}
+	}
+
+	string strCodedOriginal;
+	const char* pPM = original.c_str();
+	memset( szTmp, 0, sizeof( szTmp ) );
+	nCursor = 0;
+	for ( ; *pPM; ++pPM )
+	{
+		if ( codeTable.find( szTmp ) == codeTable.end() )
+		{
+			szTmp[ nCursor ] = *pPM;
+			++nCursor;
+		}
+		else
+		{
+			strCodedOriginal.push_back( codeTable[ szTmp ] );
+			memset( szTmp, 0, sizeof( szTmp ) );
+			nCursor = 0;
+		}
+	}
+
+	if ( *szTmp != NULL )	// 原始字串有剩余没有编码.
+	{
+//		g_HashTable[ nHashValue ] = false;
+
+		return false;
+	}
+
+	crypt = strCodedCrypt;
+	original = strCodedOriginal;
+
+	return true;
+}
 
 int main()
 {
@@ -95,8 +335,43 @@ int main()
 		return 0;
 	}
 
+	char szCrypt[ MaxCharacterNum+1 ] = {0};
+	string crypt;
+	fin.getline( szCrypt, sizeof( szCrypt ) );
+	crypt = szCrypt;
 
+	bool bOk = true;
 
+	// 判断长度.
+	bOk &= crypt.size() >= g_OriginalMessage.size();
+	bOk &= ( ( ( crypt.size() - g_OriginalMessage.size() ) % 3 ) == 0 );
+
+	if ( bOk )
+	{
+		// 对字符串重新进行编码.
+//		bOk = Recode( crypt, g_OriginalMessage );
+		
+		g_HashTable = new bool[ HASH_TABLE_SIZE + 1 ];
+		memset( g_HashTable, true, sizeof( g_HashTable[0] ) * ( HASH_TABLE_SIZE+1 ) ); 
+
+		if( bOk )
+			bOk = dfs( crypt.c_str(), g_OriginalMessage.c_str(), g_OriginalMessage.c_str() + g_OriginalMessage.size() - 1 );
+	
+		delete g_HashTable;
+		g_HashTable = 0;
+	}
+
+	
+	
+	if ( !bOk )
+	{
+		fout << 0 << " " << 0 << endl;
+	}
+	else
+	{
+		int nEncryptionNum = ( crypt.length() - PlanCharacterNum ) / 3;
+		fout << 1 << " " << nEncryptionNum << endl;
+	}
 
 #ifdef THINKINGL
 
