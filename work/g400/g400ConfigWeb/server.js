628a13d7006201163fdf69c5566f611879df74ca
thinkingzx <thinkingzx@076e9d56-d92b-0410-8186-f370c9e58ff5>
2011-02-12 10:40:33 +0000

    g400 : ci

---
+++ work/g400/g400ConfigWeb/server.js
@@ -0,0 +1,40 @@
+﻿//封装消息
+var CMonMsg =
+{
+    id: "",
+    msgBody: [],
+
+    setId: function (strId) {
+        this.id = strId;
+    },
+    setMsgNode: function (name, value) {
+        var strNode = "<" + name + ">" + value + "</" + name + ">";
+        this.msgBody.push(strNode);
+    },
+
+    getHeader: function () {
+        return "<?xml version = \"1.0\" encoding = \"gb2312\"?>"
+    },
+
+    /*getTail:function()
+    {
+    return "</xml>";	
+    },*/
+
+    getMsg: function () {
+        var body = "";
+        for (var index = 0; index < this.msgBody.length; index++) {
+            body += this.msgBody[index];
+        }
+        var msg = this.getHeader()
+		           + "<kedacomxmldata>"
+				   + "<command>" + this.id + "</command>"
+				   + "<content>"
+				   + body
+				   + "</content>"
+				   + "</kedacomxmldata>";
+        this.id = "";
+        this.msgBody.length = 0;
+        return msg;
+    }
+};
