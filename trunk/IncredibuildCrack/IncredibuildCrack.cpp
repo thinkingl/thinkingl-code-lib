4e94b2249947691459cec45ef01c013f4ced0ab7
thinkingzx <thinkingzx@076e9d56-d92b-0410-8186-f370c9e58ff5>
2012-02-01 01:36:51 +0000

    incredibuild crack 破解更新.

--- trunk/IncredibuildCrack/IncredibuildCrack.cpp
+++ trunk/IncredibuildCrack/IncredibuildCrack.cpp
@@ -51,7 +51,7 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 
 			HKEY hKey;
 			bool bSucc = true;
-			if (ERROR_SUCCESS != RegCreateKey(HKEY_CLASSES_ROOT, _T("Interface\\{7FC8EFD9-88F1-4B46-A09D-17EBCA3EFBA7}\\ProxyStubClsid32"), &hKey))
+			if (ERROR_SUCCESS != RegCreateKey(HKEY_CLASSES_ROOT, _T("Interface\\{73A03986-42CA-4801-B4FC-21E84DF177BA}\\ProxyStubClsid32"), &hKey))
 			{
 				std::wcout<<_T("Registry Open Failed!")<<std::endl;
 				system("pause");
