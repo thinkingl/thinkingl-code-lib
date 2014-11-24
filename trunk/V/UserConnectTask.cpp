9a65970331089f0bb4f1d40ce8d491ff7e3fe8e1
thinkingzx@gmail.com <thinkingzx@gmail.com@076e9d56-d92b-0410-8186-f370c9e58ff5>
2014-11-24 15:58:54 +0000

    初步建立用户连接线程池的框架.

--- trunk/V/UserConnectTask.cpp
+++ trunk/V/UserConnectTask.cpp
@@ -1,11 +1,30 @@
 #include "UserConnectTask.h"
 
 
-CUserConnectTask::CUserConnectTask(void)
+CUserConnectTask::CUserConnectTask( CVModel* pVModel, int userInternalId )
+	:m_pVModel( pVModel )
+	,m_userInternalId( userInternalId )
 {
+
 }
 
 
 CUserConnectTask::~CUserConnectTask(void)
 {
+}
+
+void CUserConnectTask::run()
+{
+	if ( m_pVModel && CUser::INVALID_USER_INTERNAL_ID != m_userInternalId )
+	{
+		CUser userInfo;
+		bool bOk = m_pVModel->GetUser( m_userInternalId, userInfo );
+		Q_ASSERT( bOk );
+
+
+	}
+	else
+	{
+		Q_ASSERT( 0 );
+	}
 }
--- trunk/V/UserConnectTask.h
+++ trunk/V/UserConnectTask.h
@@ -6,14 +6,20 @@
 *	用户连接的任务类.
 *	+by lizhixing@2014-11-23
 */
+#include "VModel.h"
 
 class CUserConnectTask :
 	public QRunnable
 {
 public:
-	CUserConnectTask(void);
+	CUserConnectTask( CVModel* pVModel, int userInternalId );
 	virtual ~CUserConnectTask(void);
 
+	virtual void run();
 
+private:
+	int m_userInternalId;
+
+	CVModel* m_pVModel;
 };
 
