736bfa1812877f38d9bc9a4266d5e9c9fdf484c6
thinkingzx@gmail.com <thinkingzx@gmail.com@076e9d56-d92b-0410-8186-f370c9e58ff5>
2014-11-29 21:38:07 +0000

    用户连接模型完成.

--- V/User.cpp
+++ V/User.cpp
@@ -4,6 +4,7 @@
 CUser::CUser(void)
 	: m_tempInternalId( INVALID_USER_INTERNAL_ID )
 	, m_port( 0 )
+	, m_pUserSocket( 0 )
 {
 }
 
@@ -11,6 +12,7 @@ CUser::CUser( int id, const string& ip, int port )
 	: m_tempInternalId( id )
 	, m_ip( ip )
 	, m_port( port )
+	, m_pUserSocket( 0 )
 {
 }
 
@@ -37,4 +39,39 @@ int CUser::GetTempInternalId() const
 void CUser::SetTempInternalId( int id )
 {
 	m_tempInternalId = id;
+}
+
+void CUser::Work()
+{
+	if ( NULL == m_pUserSocket )
+	{
+		m_pUserSocket = new QTcpSocket( this );
+
+		connect( m_pUserSocket, SIGNAL( error(QAbstractSocket::SocketError) ), SLOT( OnError( QAbstractSocket::SocketError ) ) );
+		connect( m_pUserSocket, SIGNAL( connected() ), SLOT( OnConnect()) );
+		connect( m_pUserSocket, SIGNAL( stateChanged( QAbstractSocket::SocketState ) ), SLOT( OnStateChanged( QAbstractSocket::SocketState ) ) ) ;
+
+		m_pUserSocket->connectToHost( this->GetIP().c_str(), this->GetPort() );
+	}
+	
+}
+
+void CUser::OnConnect()
+{
+	qDebug() << "Connect Ok!";
+
+}
+
+void CUser::OnError( QAbstractSocket::SocketError err )
+{
+	qDebug() << "Connect err! er: " << err;
+
+	delete m_pUserSocket;
+	m_pUserSocket = NULL;
+
+}
+
+void CUser::OnStateChanged( QAbstractSocket::SocketState st )
+{
+	qDebug() << "Socket state changed! st:" << st;
 }
