75f13ba5e8131af83b0576d1f0aba0147dbf58ed
thinkingl <thinkingzx@gmail.com>
2015-06-01 18:39:49 +0800

    XPlayer工程创建

---
+++ Study/XPlayer/XPlayer/XPlayer.h
@@ -0,0 +1,32 @@
+
+// XPlayer.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CXPlayerApp: 
+// 有关此类的实现，请参阅 XPlayer.cpp
+//
+
+class CXPlayerApp : public CWinApp
+{
+public:
+	CXPlayerApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CXPlayerApp theApp;
